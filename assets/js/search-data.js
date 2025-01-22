b697c2572b47281ea716b3457247b9c69dea33ab
vaishnavvrao <59329153+vaishnavvrao@users.noreply.github.com>
2025-01-22 13:46:52 +0000

    Deploying to gh-pages from @ vaishnavvrao/vaishnavvrao.github.io@947b6e4f68ae0422fb1d44a00cb0a985ceb8e028 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "A growing collection of projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
